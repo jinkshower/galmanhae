8ab06534ae9ac5ffe3a8e8d193dbe79fc69d4e03
jinkshower <askthem@naver.com>
2024-08-06 11:39:19 +0900

    initial commit

---
+++ src/main/java/hiyen/galmanhae/GalmanhaeApplication.java
@@ -0,0 +1,13 @@
+package hiyen.galmanhae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalmanhaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalmanhaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hiyen/galmanhae/GalmanhaeApplicationTests.java
@@ -0,0 +1,13 @@
+package hiyen.galmanhae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalmanhaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
