da92394a3d95836d6a04ab431f673bbdf0103524
jinkshower <askthem@naver.com>
2024-08-10 17:54:22 +0900

    feat: Weather 구현 및 생성,기본 null예외 테스트 추가

---
+++ src/main/java/hiyen/galmanhae/place/domain/Place.java
@@ -0,0 +1,90 @@
+package hiyen.galmanhae.place.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import hiyen.galmanhae.place.domain.vo.Congestion;
+import hiyen.galmanhae.place.domain.vo.Location;
+import hiyen.galmanhae.place.domain.vo.Weather;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.function.Predicate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Place {
+
+	@Id
+	@Column(name = "place_id")
+	@JsonIgnore
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "place_name")
+	private String name;
+
+	@Embedded
+	private Location location;
+
+	@Embedded
+	private Weather weather;
+
+	@Embedded
+	private Congestion congestion;
+
+	@Column(name = "go_out_level")
+	@Enumerated(value = EnumType.STRING)
+	private GoOutLevel goOutLevel;
+
+	@Builder
+	public Place(
+		final String name,
+		final Location location,
+		final Weather weather,
+		final Congestion congestion,
+		final GoOutLevel goOutLevel) {
+		this(null, name, location, weather, congestion, goOutLevel);
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public enum GoOutLevel {
+		LOW("다른 장소를 찾아 볼까요?", score -> score <= 40),
+		MEDIUM("무난해요", score -> score <= 70),
+		HIGH("외출하기 좋아요!", score -> score > 70);
+
+		private final String description;
+
+		private final Predicate<Integer> condition;
+
+		public static GoOutLevel of(final Weather weather, final Congestion congestion) {
+			return Arrays.stream(values())
+				.filter(level -> level.condition.test(weather.getScore() + congestion.getScore()))
+				.findFirst()
+				.orElseThrow(() -> {
+					log.warn("적절한 외출 등급을 찾지 못했습니다. 날씨: {}, 혼잡도: {} 시간: {}", weather, congestion, LocalDateTime.now());
+					return new IllegalArgumentException();
+				});
+		}
+	}
+}
