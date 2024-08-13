d04323eb70eed232d19374700b8467df13ade68a
jinkshower <askthem@naver.com>
2024-08-13 21:28:08 +0900

    chore: DTO -> Response로 변경 및 final 키워드 추가

--- src/main/java/hiyen/galmanhae/dataprocess/client/WeatherClient.java
+++ src/main/java/hiyen/galmanhae/dataprocess/client/WeatherClient.java
@@ -1,6 +1,6 @@
 package hiyen.galmanhae.dataprocess.client;
 
-import hiyen.galmanhae.dataprocess.dto.WeatherDTO;
+import hiyen.galmanhae.dataprocess.dto.WeatherResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface WeatherClient {
 
 	@GetMapping("/getVilageFcst?serviceKey=haOR%2F%2BX0ZiSttZyEP6tIGmnLnbOzCvyOL8RCjZOmg23EGAGwEps1K22f%2FiPk8F75oVRxarkMET6T1d1dLoGXTw%3D%3D&numOfRows=10&pageNo=1&dataType=JSON")
-	WeatherDTO fetch(
+	WeatherResponse fetch(
 		@RequestParam("base_date") String baseDate,
 		@RequestParam("base_time") String baseTime,
 		@RequestParam("nx") String nx,
--- src/main/java/hiyen/galmanhae/dataprocess/dto/CongestionResponse.java
+++ src/main/java/hiyen/galmanhae/dataprocess/dto/CongestionResponse.java
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import hiyen.galmanhae.dataprocess.json.CongestionDeserializer;
 
 @JsonDeserialize(using = CongestionDeserializer.class)
-public record CongestionDTO(
+public record CongestionResponse(
 
-	String areaName,
 	String congestionLevel,
 	String populationMin
 ) {
