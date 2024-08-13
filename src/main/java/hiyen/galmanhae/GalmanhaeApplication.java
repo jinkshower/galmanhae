98444dc091d79c39fe2a14d524daf433d5cf8f95
jinkshower <askthem@naver.com>
2024-08-13 20:35:26 +0900

    chore: 외부 API 호출을 위한 OpenFeign 의존성 추가

--- src/main/java/hiyen/galmanhae/GalmanhaeApplication.java
+++ src/main/java/hiyen/galmanhae/GalmanhaeApplication.java
@@ -2,8 +2,10 @@ package hiyen.galmanhae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GalmanhaeApplication {
 
     public static void main(String[] args) {
