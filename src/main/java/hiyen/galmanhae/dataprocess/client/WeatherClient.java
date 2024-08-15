99a9918c5d20b15042ae978169622b644c317f52
jinkshower <askthem@naver.com>
2024-08-15 13:24:26 +0900

    polish: 쓰지 않는 로그, import, 주석 정리

--- src/main/java/hiyen/galmanhae/dataprocess/client/WeatherClient.java
+++ src/main/java/hiyen/galmanhae/dataprocess/client/WeatherClient.java
@@ -3,6 +3,7 @@ package hiyen.galmanhae.dataprocess.client;
 import hiyen.galmanhae.dataprocess.client.WeatherClient.WeatherClientFallbackFactory;
 import hiyen.galmanhae.dataprocess.client.response.WeatherResponse;
 import hiyen.galmanhae.dataprocess.exception.DataProcessCheckedException.FailFetchAPIException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 	url = "${client.weather.url}",
 	fallbackFactory = WeatherClientFallbackFactory.class
 )
+@CircuitBreaker(name = "circuit")
 public interface WeatherClient {
 
 	@GetMapping("/getVilageFcst?serviceKey=${client.weather.service-key}&numOfRows=10&pageNo=1&dataType=JSON")
@@ -25,13 +27,13 @@ public interface WeatherClient {
 		@RequestParam("ny") String ny
 	) throws FailFetchAPIException;
 
+	//TODO 예외 처리 방법 재고려 필요
 	@Slf4j
 	@Component
 	class WeatherClientFallbackFactory implements FallbackFactory<WeatherClient> {
 
 		@Override
 		public WeatherClient create(final Throwable cause) {
-			log.error("oooooooo000000000000000000-3333333333342-93402394u83209480932840");
 			return (baseDate, baseTime, nx, ny) -> {
 				log.warn("Client에서 정보를 받지 못했습니다. baseDate: {}, baseTime: {}, nx: {}, ny: {}", baseDate, baseTime, nx, ny);
 				throw new FailFetchAPIException(cause);
