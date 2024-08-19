205c4d37ecdaf9a43c88debb16f83a8ed45b3401
jinkshower <askthem@naver.com>
2024-08-19 14:23:59 +0900

    feat: 람베르트 변환 메서드 구현
    기상청 격자 x,y를 계산하기 위해 위도, 경도를 받아 람베르트 좌표계로 변환시키는 기능

---
+++ src/test/java/hiyen/galmanhae/dataprocess/application/LambertCoordinateConverterTest.java
@@ -0,0 +1,25 @@
+package hiyen.galmanhae.dataprocess.application;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LambertCoordinateConverterTest {
+
+	private final LambertCoordinateConverter lambertCoordinateConverter = new LambertCoordinateConverter();
+
+	@DisplayName("위도와 경도를 람베르트 X,Y로 변환할 수 있다")
+	@Test
+	void success() {
+		final double latitude = 37.5112843816696;
+		final double longitude = 127.06005155384705;
+
+		int[] convert = lambertCoordinateConverter.convert(latitude, longitude);
+
+		assertThat(convert).isNotNull();
+		assertThat(convert).hasSize(2);
+		assertThat(convert[0]).isEqualTo(62);
+		assertThat(convert[1]).isEqualTo(127);
+	}
+}
