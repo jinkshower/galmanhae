cc05eb9fae06e9b33437f9a6b5842d202201e4bb
jinkshower <askthem@naver.com>
2024-08-20 14:26:15 +0900

    test: DataParser 테스트 추가

--- src/test/java/hiyen/galmanhae/dataprocess/util/DataParserTest.java
+++ src/test/java/hiyen/galmanhae/dataprocess/util/DataParserTest.java
@@ -2,8 +2,10 @@ package hiyen.galmanhae.dataprocess.util;
 
 import static org.assertj.core.api.Assertions.*;
 
+import hiyen.galmanhae.place.domain.placeinfo.PlaceInfo;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,24 @@ class DataParserTest {
 			Map<String, byte[]> fileMap = dataParser.processZipFile(inputStream);
 			assertThat(fileMap).isNotEmpty();
 			assertThat(fileMap).containsKeys("test_location.dbf", "test_location.shp", "test_location.shx");
+		}
+	}
+
+	@DisplayName("Map을 PlaceInfo로 변환한다")
+	@Test
+	void convertMapToPlaceInfo() throws IOException {
+		final ClassPathResource resource = new ClassPathResource("test_location.zip");
+
+		try (InputStream inputStream = resource.getInputStream()) {
+			Map<String, byte[]> fileMap = dataParser.processZipFile(inputStream);
+			List<PlaceInfo> actual = dataParser.parse(fileMap, "test_location");
+
+			assertThat(actual).isNotEmpty();
+			assertThat(actual.get(0).areaInfo().areaName()).isEqualTo("강남 MICE 관광특구");
+			assertThat(actual.get(0).locationInfo().latitude()).isEqualTo("37.5112843816696");
+			assertThat(actual.get(0).locationInfo().longitude()).isEqualTo("127.06005155384705");
+			assertThat(actual.get(0).weatherInfo().weatherX()).isEqualTo("62");
+			assertThat(actual.get(0).weatherInfo().weatherY()).isEqualTo("127");
 		}
 	}
 }
