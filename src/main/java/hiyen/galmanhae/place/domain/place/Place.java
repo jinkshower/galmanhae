6c8f1c7395a65f50e9824955d8821f724d00a935
jinkshower <askthem@naver.com>
2024-08-25 14:55:51 +0900

    refactor: Place에서 Location을 삭제한다

--- src/main/java/hiyen/galmanhae/place/domain/place/Place.java
+++ src/main/java/hiyen/galmanhae/place/domain/place/Place.java
@@ -12,7 +12,6 @@ import org.springframework.util.StringUtils;
 @Slf4j
 public record Place(
 	String name,
-	Location location,
 	Weather weather,
 	Congestion congestion,
 	GoOutLevel goOutLevel
--- src/main/java/hiyen/galmanhae/place/entity/BaseEntity.java
+++ src/main/java/hiyen/galmanhae/place/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
@@ -14,4 +15,8 @@ public abstract class BaseEntity {
 	@CreatedDate
 	@Column(name = "created_at", updatable = false)
 	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
 }
