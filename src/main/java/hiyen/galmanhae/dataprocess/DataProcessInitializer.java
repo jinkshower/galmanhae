ea34d836c9daed3945edd462d3e5aec902212cc5
jinkshower <askthem@naver.com>
2024-08-25 21:54:48 +0900

    fix: feign 클라이언트 호출을 application load가 끝난 시점으로 늦춤

--- src/main/java/hiyen/galmanhae/dataprocess/DataProcessInitializer.java
+++ src/main/java/hiyen/galmanhae/dataprocess/DataProcessInitializer.java
@@ -1,9 +1,10 @@
 package hiyen.galmanhae.dataprocess;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +21,7 @@ public class DataProcessInitializer {
 	 * 애플리케이션 시작시 장소 목록을 다운로드 이후 DB에 저장
 	 * 해당 장소 목록을 읽어 외부 API를 호출하여 데이터를 가져오고 Place 테이블에 저장
 	 */
-	@PostConstruct
+	@EventListener(ApplicationReadyEvent.class)
 	public void init() {
 		log.info("애플리케이션이 시작되고 데이터 처리를 시작합니다");
 		placeInfoDataProcessor.process();
