ab6083296afa786e271e2257c46434f6eb602d13
jinkshower <askthem@naver.com>
2024-08-27 20:09:43 +0900

    refactor: 점수 계산 비즈니스 로직을 dataquery.domain으로 옮기고 PlaceDetails 객체 완성

--- src/main/java/hiyen/galmanhae/dataquery/domain/WeatherLevel.java
+++ src/main/java/hiyen/galmanhae/dataquery/domain/WeatherLevel.java
@@ -1,6 +1,6 @@
-package hiyen.galmanhae.place.domain.place;
+package hiyen.galmanhae.dataquery.domain;
 
-import java.time.LocalDateTime;
+import hiyen.galmanhae.data.domain.Weather;
 import java.util.Arrays;
 import java.util.function.Predicate;
 import lombok.Getter;
@@ -13,9 +13,10 @@ public record WeatherLevel(
 	WeatherTempGrade weatherTempGrade,
 	Integer weatherScore
 ) {
-	public static WeatherLevel of(final Double weatherTemp, final Double weatherRaining) {
-		final WeatherRainingGrade rainingGrade = WeatherRainingGrade.of(weatherRaining);
-		final WeatherTempGrade tempGrade = WeatherTempGrade.of(weatherTemp);
+
+	public static WeatherLevel of(final Weather weather) {
+		final WeatherTempGrade tempGrade = WeatherTempGrade.of(weather.temperature());
+		final WeatherRainingGrade rainingGrade = WeatherRainingGrade.of(weather.rainingProbability());
 		final int score = rainingGrade.getScore() + tempGrade.getScore();
 
 		return new WeatherLevel(rainingGrade, tempGrade, score);
@@ -28,9 +29,10 @@ public record WeatherLevel(
 	@Getter
 	@RequiredArgsConstructor
 	public enum WeatherRainingGrade {
-		SUNNY("맑아요", 40, probability -> probability <= 10),
-		CLOUDY("흐려요", 20, probability -> probability <= 50),
-		RAINY("비와요", 5, probability -> probability > 50);
+		SUNNY("맑아요", 40, probability -> probability >= 0 && probability <= 10),
+		CLOUDY("흐려요", 20, probability -> probability > 10 && probability <= 50),
+		RAINY("비와요", 5, probability -> probability > 50),
+		NONE(" ", 0, probability -> true);
 
 		private final String description;
 		private final int score;
@@ -41,21 +43,19 @@ public record WeatherLevel(
 			return Arrays.stream(values())
 				.filter(grade -> grade.condition.test(rainingProbability))
 				.findFirst()
-				.orElseThrow(() -> {
-					log.warn("적절한 강수 등급을 찾지 못했습니다. 강수확률: {}, 시간: {}", rainingProbability, LocalDateTime.now());
-					return new IllegalArgumentException();
-				});
+				.orElse(NONE);
 		}
 	}
 
 	@Getter
 	@RequiredArgsConstructor
 	public enum WeatherTempGrade {
-		COLD("춥고", 5, temp -> temp <= 5),
-		CHILLY("쌀쌀하고", 20, temp -> temp <= 10),
-		MILD("시원하고", 40, temp -> temp <= 20),
-		WARM("따뜻하고", 20, temp -> temp <= 30),
-		HOT("덥고", 5, temp -> temp > 30);
+		COLD("춥고", 5, temp -> temp >= 0 && temp <= 5),
+		CHILLY("쌀쌀하고", 20, temp -> temp > 5 && temp <= 10),
+		MILD("시원하고", 40, temp -> temp > 10 && temp <= 20),
+		WARM("따뜻하고", 20, temp -> temp > 20 && temp <= 30),
+		HOT("덥고", 5, temp -> temp > 30),
+		NONE(" ", 0, temp -> true);
 
 		private final String description;
 		private final int score;
@@ -66,10 +66,7 @@ public record WeatherLevel(
 			return Arrays.stream(values())
 				.filter(grade -> grade.condition.test(weatherTemp))
 				.findFirst()
-				.orElseThrow(() -> {
-					log.warn("적절한 기온 등급을 찾지 못했습니다. 기온: {}, 시간: {}", weatherTemp, LocalDateTime.now());
-					return new IllegalArgumentException();
-				});
+				.orElse(NONE);
 		}
 	}
 }
