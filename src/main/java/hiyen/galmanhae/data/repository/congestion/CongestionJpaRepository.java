ba38417c79a7f766b88a709179a74e8b74a47886
jinkshower <askthem@naver.com>
2024-08-29 21:54:55 +0900

    polish: sql 텍스트 블록 사용, prod환경 테스트 disabled 처리

--- src/main/java/hiyen/galmanhae/data/repository/congestion/CongestionJpaRepository.java
+++ src/main/java/hiyen/galmanhae/data/repository/congestion/CongestionJpaRepository.java
@@ -8,6 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface CongestionJpaRepository extends JpaRepository<CongestionEntity, Long> {
 
-	@Query(value = "SELECT * FROM congestion c WHERE c.place_id = :placeId ORDER BY c.measured_at DESC LIMIT 1", nativeQuery = true)
+	@Query(value = """
+		SELECT * 
+		FROM congestion c 
+		WHERE c.place_id = :placeId 
+		ORDER BY c.measured_at DESC 
+		LIMIT 1
+		""", nativeQuery = true)
 	Optional<CongestionEntity> findMostRecentByPlaceId(@Param("placeId") Long placeId);
 }
