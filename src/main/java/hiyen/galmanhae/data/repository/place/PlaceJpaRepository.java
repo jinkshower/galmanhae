52cf73066f42685f036fda401f52d2c5b2b4741f
jinkshower <askthem@naver.com>
2024-08-29 17:35:49 +0900

    feat: 데이터 업데이트시 일관성을 지키기 위해 Transaction사용

--- src/main/java/hiyen/galmanhae/data/repository/place/PlaceJpaRepository.java
+++ src/main/java/hiyen/galmanhae/data/repository/place/PlaceJpaRepository.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PlaceJpaRepository extends JpaRepository<PlaceEntity, Long> {
 
 	List<PlaceEntity> findAll();
 
 	@Modifying(clearAutomatically = true)
+	@Transactional
 	@Query("delete from PlaceEntity p")
 	void deleteAll();
 
