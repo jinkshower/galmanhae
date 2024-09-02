b85a1669e641c104374f902279e9cfee98b0cf67
jinkshower <askthem@naver.com>
2024-09-02 13:10:57 +0900

    feat: weather, congestion 엔티티 pk 변경

--- src/main/java/hiyen/galmanhae/data/domain/Congestion.java
+++ src/main/java/hiyen/galmanhae/data/domain/Congestion.java
@@ -1,13 +1,13 @@
 package hiyen.galmanhae.data.domain;
 
 public record Congestion(
-
+	Long placeId,
 	int currentPeople,
 	String congestionIndicator
 
 ) {
 
 	public static Congestion of(final int currentPeople, final String congestionIndicator) {
-		return new Congestion(currentPeople, congestionIndicator);
+		return new Congestion(null, currentPeople, congestionIndicator);
 	}
 }
