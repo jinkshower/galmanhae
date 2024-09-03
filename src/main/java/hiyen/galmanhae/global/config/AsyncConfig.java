20785d3124647e501c61780485c45b91308462b7
jinkshower <askthem@naver.com>
2024-09-03 17:03:16 +0900

    feat: 스레드 풀 사이즈 고정

--- src/main/java/hiyen/galmanhae/global/config/AsyncConfig.java
+++ src/main/java/hiyen/galmanhae/global/config/AsyncConfig.java
@@ -1,10 +1,10 @@
 package hiyen.galmanhae.global.config;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
@@ -12,8 +12,13 @@ public class AsyncConfig {
 
 	public static final String WEATHER_CONGESTION_API_THREAD_POOL = "weatherCongestionApiThreadPool";
 
+	private static final int POOL_SIZE = 20;
+
 	@Bean(name = WEATHER_CONGESTION_API_THREAD_POOL)
 	public Executor weatherCongestionAPIThreadPool() {
-		return Executors.newCachedThreadPool();
+		final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(POOL_SIZE);
+		executor.setMaxPoolSize(POOL_SIZE);
+		return executor;
 	}
 }
